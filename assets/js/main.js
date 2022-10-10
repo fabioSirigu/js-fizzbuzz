3585edd9bc43e45c8515fda0cd9bf16be60974ec
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-10 14:19:19 +0200

    add fizzbuzz console log

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,19 @@ Scrivi un programma che stampi in console i numeri da 1 a 100,
 ma che per i multipli di 3 stampi “Fizz” al posto del numero
 e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
+
+for (i=1; i<=100; i++) {
+  // se sono multipli di 3 stampiamo Fizz
+  // se sono multipli di 5 stampiamo Buzz
+  // se sono multipli sia di 3 che di 5 stampiamo FizzBuzz
+  
+  if ( i % 3 ===0) { 
+      console.log('FizzBuzz');
+  } else if ( i % 5 === 0 ) { 
+      console.log('Fizz');
+  } else if ( i % 3 ===0 && i % 5 === 0) { 
+      console.log('Buzz');
+  } else { 
+      console.log(i); 
+  }
+}
