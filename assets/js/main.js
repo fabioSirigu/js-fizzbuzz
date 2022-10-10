e586afbe84c5b79360d2a6f365cb13acd7f6de0a
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-10 17:08:08 +0200

    add bonus 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,12 +37,22 @@ for (i=1; i<=100; i++) {
       
       if ( i % 3 ===0 && i % 5 === 0) {
             boxNumber.append ('FizzBuzz');
+            boxNumber.style.background = "rgb(34,193,195)";
+            boxNumber.style.background = "linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)";
       } else if ( i % 5 === 0 ) { 
             boxNumber.append ('Buzz');
+            boxNumber.style.background = "rgb(34,193,195)";
+            boxNumber.style.background = "linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,45,178,1) 100%)";
+
       } else if ( i % 3 ===0 ) {
             boxNumber.append ('Fizz');
+            boxNumber.style.background = "rgb(34,193,195)";
+            boxNumber.style.background = "linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(45,105,253,1) 100%)";
+
       } else { 
-            boxNumber.append (i) 
+            boxNumber.append (i);
+            boxNumber.style.background = "rgb(238,174,202)";
+            boxNumber.style.background = "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)";
       }
 }
 
